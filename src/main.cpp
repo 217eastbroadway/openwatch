e9aa7f8287f94cfe3816fb2d5ab3634ebab0f96b
217eastbroadway <newyorkedoardo@gmail.com>
2024-10-02 07:17:05 +0200

    Trying out images

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@
 int main(){
     InitWindow(800, 800, "[openwatch] | Main Window");
 
+    Image img = LoadImage("assets/test.png");
+    Texture2D texture = LoadTextureFromImage(img);
+
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        std::string s = "FPS: " + GetFPS();
+        std::string s = "FPS: ";
         DrawText(s.c_str(), 190, 200, 20, LIGHTGRAY);
+
+        
+        DrawTexture(texture, 350, 350, WHITE);
         EndDrawing();
     }
 
