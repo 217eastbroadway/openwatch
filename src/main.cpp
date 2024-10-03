749cf8b93069a0d459c03075d1d2027494c522c6
217eastbroadway <newyorkedoardo@gmail.com>
2024-10-03 07:41:24 +0200

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include <raylib/raylib.h>
 #include <string>
 #include <cstdlib>
+#include <iostream>
+
+std::string getTimeString() {
+    double time = GetTime() + 3650;
+
+    int hour = 0;
+    int minute = 0;
+    int second = time;
+    
+
+    std::string s = std::to_string(hour) + ":" + std::to_string(minute) + ":" + std::to_string(second);
+    return s;
+}
 
 int main(){
-    InitWindow(800, 800, "[openwatch] | Main Window");
-
-    Image img = LoadImage("assets/test.png");
-    Texture2D texture = LoadTextureFromImage(img);
+    InitWindow(800, 550, "[openwatch] | Main Window");
+    Font f = LoadFontEx("assets/fonts/helvetica_neue_65.ttf", 30, 0, NULL);
 
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        std::string s = "FPS: ";
-        DrawText(s.c_str(), 190, 200, 20, LIGHTGRAY);
+        DrawTextEx(f, getTimeString().c_str(), {10, 10}, 30, 0, BLACK);
 
-        
-        DrawTexture(texture, 350, 350, WHITE);
         EndDrawing();
     }
 
