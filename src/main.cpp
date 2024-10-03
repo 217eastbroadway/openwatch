502bcdb80bbae5f1394ccb415200de5876ed0865
217eastbroadway <newyorkedoardo@gmail.com>
2024-10-03 10:09:32 +0200

    git

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,41 @@
 #include <string>
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 
-std::string getTimeString() {
-    double time = GetTime() + 3650;
+std::string getTimeString(time_t time) {
+    int seconds = (int)time;
+    int minutes = 0;
+    int hours = 0;
 
-    int hour = 0;
-    int minute = 0;
-    int second = time;
-    
+    if(seconds >= 60) {
+        minutes = seconds / 60;
+        seconds %= 60;
+    }
 
-    std::string s = std::to_string(hour) + ":" + std::to_string(minute) + ":" + std::to_string(second);
+    if(minutes >= 60) {
+        hours = minutes / 60;
+        minutes %= 60;
+    }
+
+    std::string s = hours < 10 ? '0' + std::to_string(hours) : std::to_string(hours);
+    s += ':';
+    s += minutes < 10 ? '0' + std::to_string(minutes) : std::to_string(minutes);
+    s += ':';
+    s += seconds < 10 ? '0' + std::to_string(seconds) : std::to_string(seconds);
+
     return s;
 }
 
 int main(){
     InitWindow(800, 550, "[openwatch] | Main Window");
-    Font f = LoadFontEx("assets/fonts/helvetica_neue_65.ttf", 30, 0, NULL);
+    Font f = LoadFontEx("assets/fonts/helvetica_neue_65.ttf", 80, 0, NULL);
 
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawTextEx(f, getTimeString().c_str(), {10, 10}, 30, 0, BLACK);
+        DrawTextEx(f, getTimeString().c_str(), {10, 10}, 80, 0, BLACK);
 
         EndDrawing();
     }
